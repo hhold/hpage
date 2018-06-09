cbdad2e61bbb8ccd26f8dc48470c03ff8a5730f4
Hitesh Nair <zombiesarcasm@gmail.com>
2018-06-09 19:08:03 +0530

    Add files via upload

---
+++ src/type.js
@@ -0,0 +1,9 @@
+
+
+
+  var typed = new Typed('.element', {
+    strings: [, "We are the music makers, and we are the dreamers of the dreams."],
+    backSpeed: 40,
+                      typeSpeed: 50,
+
+  });
